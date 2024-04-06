feba43fdfe51683898fbe28ff75429de5333e7a0
Angelo Xavier Pires <Angelo.XavierPires@edu.sait.ca>
2024-04-05 22:39:40 -0600

    Adicionar arquivos de projeto.

---
+++ lab9DataBaseConnection/Courses.cs
@@ -0,0 +1,78 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace lab9DataBaseConnection
+{
+    public class Courses
+    {
+        private string connectionString = "Your_Connection_String_Here";
+
+        public void DisplayAllCourses()
+        {
+            string query = "SELECT * FROM Courses";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                MySqlCommand command = new MySqlCommand(query, connection);
+                connection.Open();
+
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"CourseId: {reader["CourseId"]}, Name: {reader["Name"]}, Credits: {reader["Credits"]}");
+                    }
+                }
+            }
+        }
+
+
+        public void AddCourse(int courseId, string name, int credits)
+        {
+            string query = "INSERT INTO Courses (CourseId, Name, Credits) VALUES (@CourseId, @Name, @Credits)";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                MySqlCommand command = new MySqlCommand(query, connection);
+                command.Parameters.AddWithValue("@CourseId", courseId);
+                command.Parameters.AddWithValue("@Name", name);
+                command.Parameters.AddWithValue("@Credits", credits);
+                connection.Open();
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void UpdateCourse(int courseId, string newName, int newCredits)
+        {
+            string query = "UPDATE Courses SET Name = @Name, Credits = @Credits WHERE CourseId = @CourseId";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                MySqlCommand command = new MySqlCommand(query, connection);
+                command.Parameters.AddWithValue("@CourseId", courseId);
+                command.Parameters.AddWithValue("@Name", newName);
+                command.Parameters.AddWithValue("@Credits", newCredits);
+                connection.Open();
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void DeleteCourse(int courseId)
+        {
+            string query = "DELETE FROM Courses WHERE CourseId = @CourseId";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                MySqlCommand command = new MySqlCommand(query, connection);
+                command.Parameters.AddWithValue("@CourseId", courseId);
+                connection.Open();
+
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ lab9DataBaseConnection/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace lab9DataBaseConnection
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder
+            {
+                Server = "127.0.0.1",
+                UserID = "root",
+                Password = "251119-Jp",
+                Database = "curses" 
+            };
+
+            
+            Console.WriteLine(builder.ConnectionString);
+
+            
+            CourseManager courseManager = new CourseManager(builder.ConnectionString);
+
+            
+            courseManager.DisplayAllCourses();
+        }
+    }
+
+    public class CourseManager
+    {
+        private string connectionString;
+
+        public CourseManager(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public void DisplayAllCourses()
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    
+                    connection.Open();
+
+                    
+                    string query = "SELECT * FROM Courses";
+
+                    
+                    MySqlCommand command = new MySqlCommand(query, connection);
+
+                    
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"CourseId: {reader["CourseId"]}, Name: {reader["Name"]}, Credits: {reader["Credits"]}");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    
+                    Console.WriteLine("An error has occurred: " + ex.Message);
+                }
+            }
+        }
+    }
+
+}
